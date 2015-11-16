07328560417ec9968e86c892029a77a444c91b79
Charlie Gao <charles.gao.92@gmail.com>
2015-11-16 16:47:37 -0500

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/res/layout/task_pre_act_content.xml

--- app/src/main/java/com/leechangu/sweettask/login/RegisterActivity.java
+++ app/src/main/java/com/leechangu/sweettask/login/RegisterActivity.java
@@ -3,7 +3,6 @@ package com.leechangu.sweettask.login;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
