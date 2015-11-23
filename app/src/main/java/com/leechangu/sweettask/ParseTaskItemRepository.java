d156409cc1993e9ef2bc70f595ef07a2807f6657
Charlie Gao <charles.gao.92@gmail.com>
2015-11-23 16:46:16 -0500

    ParseDB all set

--- app/src/main/java/com/leechangu/sweettask/ParseTaskItemRepository.java
+++ app/src/main/java/com/leechangu/sweettask/ParseTaskItemRepository.java
@@ -7,7 +7,6 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
