0f8afd634e9dec1f7144cb91dac62a81471beba6
Charlie Gao <charles.gao.92@gmail.com>
2015-11-23 20:28:47 -0500

    before merge

--- app/src/main/java/com/leechangu/sweettask/MainActivity.java
+++ app/src/main/java/com/leechangu/sweettask/MainActivity.java
@@ -25,7 +25,6 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-//import com.leechangu.sweettask.db.TaskDb;
 import com.leechangu.sweettask.settask.TaskPreferenceActivity;
 import com.parse.ParseUser;
 
